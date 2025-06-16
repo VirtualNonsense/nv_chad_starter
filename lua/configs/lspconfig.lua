433e67de79212aa05eb156863d26c236dd36c324
Andreas Nachtmann <virtualnonsense@mailbox.org>
2025-06-16 10:31:51 +0200

    improving rust lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,22 @@
 require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "html", "cssls" }
+vim.lsp.config("rust_analyzer", {
+  cargo = {
+        allFeatures = true,
+      },
+      checkOnSave = {
+        command = "clippy",
+      },
+      procMacro = {
+        enable = true,
+      },
+})
+local servers = {
+  "html",
+  "cssls",
+  "rust_analyzer",
+  "ruff",
+  "ruff_lsp",
+}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
