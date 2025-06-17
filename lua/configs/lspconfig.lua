2bd1fc799ee9afd9b69b4d321822c7802a8aaddf
Andreas Nachtmann <virtualnonsense@mailbox.org>
2025-06-17 14:55:01 +0200

    using basedpyright and ruff

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,11 +1,48 @@
 require("nvchad.configs.lspconfig").defaults()
 local servers = {
-  "html",
-  "cssls",
-  "rust_analyzer",
-  "ruff",
-  "ruff_lsp",
+  html = {},
+  cssls = {},
+  rust_analyzer = {},
+  --pylsp = {},
+  pyright = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+      python = {
+        -- analysis = {
+        --   -- Ignore all files for analysis to exclusively use Ruff for linting
+        --   ignore = { "*" },
+        -- },
+      },
+    },
+  },
+  basedpyright = {
+    basedpyright = {
+      disableOrganizeImports = true,
+      analysis = {
+        diagnosticMode = "openFilesOnly",
+        inlayHints = {
+          callArgumentNames = true,
+        },
+      },
+    },
+  },
+  -- ruff = {},
+  ruff = {
+    -- init_options = {
+    --   settings = {
+    --     lint = {
+    --       preview = true,
+    --       enable = true,
+    --     },
+    --   },
+    -- },
+  },
 }
-vim.lsp.enable(servers)
+
+for name, opts in pairs(servers) do
+  vim.lsp.enable(name)
+  vim.lsp.config(name, opts)
+end
 
 -- read :h vim.lsp.config for changing options of lsp servers
