fa4f649cd9691b7cc67d281dc3778c52cdc4e081
Andreas Nachtmann <virtualnonsense@mailbox.org>
2025-06-18 11:41:39 +0200

    adjusted conform config to accomodate python; enabled format on save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,24 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    rust = { "rustfmt" },
+    python = {
+      -- To fix auto-fixable lint errors.
+      "ruff_fix",
+      -- To run the Ruff formatter.
+      "ruff_format",
+      -- To organize the imports.
+      "ruff_organize_imports",
+    },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
