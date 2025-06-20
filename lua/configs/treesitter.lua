6f0ffe7876921c36598c6f3748ef18466ae0515f
Andreas Nachtmann <virtualnonsense@mailbox.org>
2025-06-20 09:01:22 +0200

    moved treesitter config into configs dir

---
+++ lua/configs/treesitter.lua
@@ -0,0 +1,28 @@
+local opts = {
+  ensure_installed = { "rust", "lua", "python" },
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
+  indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = "<C-s>",
+      node_decremental = "<C-backspace>",
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+  },
+}
+
+return opts
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -142,35 +142,11 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects", -- falls du @function.inner etc. nutzt
-    },
-    opts = {
-      ensure_installed = { "rust", "lua", "python" },
-      highlight = { enable = true, additional_vim_regex_highlighting = false },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = "<C-s>",
-          node_decremental = "<C-backspace>",
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-      },
+      "nvim-treesitter/nvim-treesitter-context",
     },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      local config = require "configs.treesitter"
+      require("nvim-treesitter.configs").setup(config)
     end,
   },
 }
